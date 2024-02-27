5e08607c89f46511f5c00717ebcd8d62b1796b7b
Leonardo <leonardo.hteixeira@outlook.com.br>
2024-02-27 19:01:43 -0300

    Criado e estilizado modal

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,15 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
+import Modal from './components/Modal';
+
 
 //CSS
 import styles from "./App.module.css";
 
 //Interface
 import { ITask } from './interfaces/Task';
+
 
 function App() {
 
@@ -26,6 +29,7 @@ function App() {
 
   return (
     <div>
+      <Modal children={<TaskForm btnText='Editar Tarefa' taskList={taskList}/>} />
       <Header />
       <main className={styles.main}>
         <div>
